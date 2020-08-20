1f79992891f4d5c815c1c911950870a7f66b7f32
Emilia Zyśk （ジシク・エミリア） <57023406+emzys@users.noreply.github.com>
2020-08-20 21:46:24 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+const converter = document.querySelector('#converter');
+const result = document.querySelector('.result');
+const convBtn = document.querySelector('.conv');
+const resetBtn = document.querySelector('.reset');
+const changeBtn = document.querySelector('.change');
+const one = document.querySelector('.one');
+const two = document.querySelector('.two');
+let fahrenheit;
+let celsius;
+
+const swap = () => {
+    if (one.innerText === '°C') {
+        one.innerText = '°F';
+        two.innerText = '°C';
+        result.innerText = '';
+        converter.value = '';
+    } else {
+        one.innerText = '°C';
+        two.innerText = '°F';
+        result.innerText = '';
+        converter.value = '';
+    }
+};
+
+const convert = () => {
+    if (converter.value !== '') {
+        if (one.innerText === '°C') {
+            fahrenheit = converter.value * 1.8 + 32;
+            result.innerText = `${converter.value}°C equals ${fahrenheit.toFixed(1)}°F.`
+            converter.value = '';
+        } else {
+            celsius = (converter.value - 32) / 1.8;
+            result.innerText = `${converter.value}°F equals ${celsius.toFixed(1)}°C.`
+            converter.value = '';
+        }
+    } else {
+        result.innerText = 'Please enter a number before you click the CONVERT button.'
+    }
+};
+
+convBtn.addEventListener('click', convert);
+changeBtn.addEventListener('click', swap);
+
+resetBtn.addEventListener('click', function() {
+    result.innerText = '';
+    converter.value = '';
+});
